4bb00b96a2be3e72149b7b7cf92a7a2013531887
Andrea Tedeschi <andry.teddy@gmail.com>
2020-12-28 09:21:24 +0100

    Added delete key features and started x5u feature

--- demo.py
+++ demo.py
@@ -7,29 +7,38 @@ Write the function in this file, test it and when it's ready to work, move it in
 """
 
 import sys
-import urllib.parse
+import os
+import subprocess
 
 
-def url_escape(string, chars, spaces=True):
-    """
-    :param string: The string to url encode
-    :param chars: The only chars you want to escape
-
-    From a given string, the fucntions url encode only the carachters specified in the chars parameter.
-    By default, the space character is automatically appended to the chars string. You can disable this behaviour,
-    setting spaces=False.
-
-    return: The original string with the specified characters url encoded
-    """
-    if " " not in chars and spaces:
-        chars += " "
-    encoded = [urllib.parse.quote(char) for char in chars]
-    for i in range(len(chars)):
-        string = string.replace(chars[i], encoded[i])
-    return string
 
 
-if __name__ == '__main__':
 
-    final = url_escape('{"keys": [{"kty": "jwks", "e": "abaq", "alg": "rsa"}]}', "[]{}")
-    print(final)
+
+
+
+
+
+
+
+def x5u_basic(header):
+    devnull_ = open(os.devnull, 'wb')
+    download = "wget " + header['x5u']
+    download_output = subprocess.check_output(command, shell=True, stdin=devnull_, stderr=devnull_)
+    # Retrieve the right filename    TODO: Implement it in a better way
+    for file in os.listdir():
+        if file.endswith(".json"):
+            filename = file
+            break
+    else:
+        filename = header['x5u'].split("/")[-1] if header['x5u'].split("/")[-1].endswith(".json") else header['x5u'].split("/")[-2]
+    with open("testing.crt", 'r') as cert_file:
+        my_cert = "".join([line.strip() for line in cert_file if not line.startswith('---')])
+    jwks = open(filename)
+    jwks_dict = json.load(jwks)
+    jwks['x5c'] = my_cert
+    #change something else
+    file = open("crafted/jwks.json", 'w')
+    file.write(json.dumps(jwks_dict))
+    devnull_.close()
+    file.close()
