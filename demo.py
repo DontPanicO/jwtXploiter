c4e0d440ec97f1a0939c29f852b5e3b6041ac0a1
andrea.tedeschi <sviluppo6@metodogroup.it>
2021-01-07 13:46:10 +0100

    Started to implement a jwk feature

--- demo.py
+++ demo.py
@@ -8,4 +8,31 @@ Write the function in this file, test it and when it's ready to work, move it in
 
 import sys
 import os
+import base64
+import json
 import subprocess
+
+
+header = {"alg": "RS256", "typ": "jwt"}
+
+n = "something"
+e = "ABaQ"
+kid = "kid001"
+
+def generate_jwk(n, e, kid):
+    jwk = dict()
+    jwk['kty'] = "RSA"
+    jwk['kid'] = kid
+    jwk['use'] = "sig"
+    jwk['n'] = n
+    jwk['e'] = e
+    return jwk
+
+def inject_jwk(header):
+    crafted_jwk = generate_jwk(n, e, kid)
+    header['jwk'] = crafted_jwk
+    return header
+
+new_header = inject_jwk(header)
+
+print(json.dumps(new_header))
